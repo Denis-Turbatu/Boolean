f6b815c295651e023b213cdc9df09bf6c826b0ad
Denis-Turbatu <turbatudenis81@gmail.com>
2024-04-02 13:06:19 +0200

    feat: add 02-04 with 2 snacks

---
+++ Teoria/APRILE-04/02-04/snack copy/assets/js/script.js
@@ -0,0 +1,8 @@
+const par1 = prompt("parola 1: ");
+const par2 = prompt("parola 2: ");
+
+if (par1.length > par2.length) {
+    console.log(par2, par1);
+} else {
+    console.log(par1, par2);
+}
---
+++ Teoria/APRILE-04/02-04/snack/assets/js/script.js
@@ -0,0 +1,8 @@
+const num1 = parseInt(prompt("numero 1: "));
+const num2 = parseInt(prompt("numero 2: "));
+
+if (num1 > num2) {
+    console.log(num1);
+} else {
+    console.log(num2);
+}
